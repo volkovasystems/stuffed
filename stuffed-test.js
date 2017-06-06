f5d2b6fffc59f61c196b61de05919b26ca1bdbb0
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-06 18:01:16 +0800

    Updated test.

--- stuffed-test.js
+++ stuffed-test.js
@@ -5,4 +5,14 @@ const stuffed = require( "./stuffed.js" );
 
 assert.equal( stuffed( { "name": "simple" } ), true, "should be true" );
 assert.equal( stuffed( { } ), false, "should be false" );
+
+let Guest = { };
+Guest.addGuest = function addGuest( name ) {
+	console.log( name );
+};
+
+assert.equal( stuffed( Guest ), true, "should  be true" );
+
+assert.equal( stuffed( { "data": [ 1, 2, 3 ] } ), true, "should be true" );
+
 console.log( "ok" );
