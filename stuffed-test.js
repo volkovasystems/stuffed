ad8f5daaa87f69baf3f04388ea905d234a797dd5
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-04 13:31:10 +0800

    Updated server-side test.

--- stuffed-test.js
+++ stuffed-test.js
@@ -1,18 +1,9 @@
-"use strict";
 
 const assert = require( "assert" );
 const stuffed = require( "./stuffed.js" );
 
 assert.equal( stuffed( { "name": "simple" } ), true, "should be true" );
+
 assert.equal( stuffed( { } ), false, "should be false" );
-
-let Guest = { };
-Guest.addGuest = function addGuest( name ) {
-	console.log( name );
-};
-
-assert.equal( stuffed( Guest ), true, "should  be true" );
-
-assert.equal( stuffed( { "data": [ 1, 2, 3 ] } ), true, "should be true" );
 
 console.log( "ok" );
