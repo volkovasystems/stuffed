dbce8f2b858b82d0e27ef08c8a9af70c6be33dc5
VINSEVINALON\vinsevinalon <vinsevinalon@gmail.com>
2017-08-09 06:07:48 +0800

    Update client/server unit test.

--- stuffed-test.js
+++
@@ -1,9 +0,0 @@
-
-const assert = require( "assert" );
-const stuffed = require( "./stuffed.js" );
-
-assert.equal( stuffed( { "name": "simple" } ), true, "should be true" );
-
-assert.equal( stuffed( { } ), false, "should be false" );
-
-console.log( "ok" );
---
+++ stuffed.module.js
@@ -0,0 +1,81 @@
+"use strict";
+
+/*;
+	@module-license:
+		The MIT License (MIT)
+		@mit-license
+
+		Copyright (@c) 2017 Richeve Siodina Bebedor
+		@email: richeve.bebedor@gmail.com
+
+		Permission is hereby granted, free of charge, to any person obtaining a copy
+		of this software and associated documentation files (the "Software"), to deal
+		in the Software without restriction, including without limitation the rights
+		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+		copies of the Software, and to permit persons to whom the Software is
+		furnished to do so, subject to the following conditions:
+
+		The above copyright notice and this permission notice shall be included in all
+		copies or substantial portions of the Software.
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+		SOFTWARE.
+	@end-module-license
+
+	@module-configuration:
+		{
+			"package": "stuffed",
+			"path": "stuffed/stuffed.js",
+			"file": "stuffed.js",
+			"module": "stuffed",
+			"author": "Richeve S. Bebedor",
+			"eMail": "richeve.bebedor@gmail.com",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+				"Vinse Vinalon <vinsevinalon@gmail.com>"
+			],
+			"repository": "https://github.com/volkovasystems/stuffed.git",
+			"test": "stuffed-test.js",
+			"global": true
+		}
+	@end-module-configuration
+
+	@module-documentation:
+		Check if object contains something.
+	@end-module-documentation
+
+	@include:
+		{
+			"doubt": "doubt",
+			"kount": "kount",
+			"truly": "truly"
+		}
+	@end-include
+*/
+
+const doubt = require( "doubt" );
+const kount = require( "kount" );
+const truly = require( "truly" );
+
+const stuffed = function stuffed( object ){
+	/*;
+		@meta-configuration:
+			{
+				"object:required": "object"
+			}
+		@end-meta-configuration
+	*/
+
+	if( typeof object != "object" || doubt( object, AS_ARRAY ) ){
+		throw new Error( "invalid object" );
+	}
+
+	return ( truly( object ) && kount( object ) != 0 );
+};
+
+module.exports = stuffed;
